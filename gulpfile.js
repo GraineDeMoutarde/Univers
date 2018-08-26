90d0080f3166c28211fe14d5c3ed2d78b3b7d8f7
taserflanger <pascal.nardi@gmail.com>
2018-08-26 19:20:22 +0200

    Revert "création du dossier site web"
    This reverts commit 7f6a2ef864665db9350ffe2602f8aaf3fb0ada34.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,14 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var tap = require('gulp-tap');
-var MarkdownIt = require('markdown-it');
+var markdown = require('gulp-markdown-it');
 
-var md = new MarkdownIt();
-
-md.use(require('markdown-it-container'), 'title', {
-    render: function(tokens, idx) {
-        var content = tokens[idx];
-        console.log(content);
-        console.log(content.info.trim());
-        return "";
-        // return "<!DOCTYPE html>\n        <html lang=\"en\">\n        <head>\n            <meta charset=\"UTF-8\">\n            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n            <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n            <link rel=\"stylesheet\" href=\"./node_modules/bootstrap/dist/css/bootstrap.min.css\">\n            <title>" + content + "</title>\n        </head>"
-    }
-})
-
-gulp.task('build', function() {
+gulp.task('markdown', function() {
     return gulp.src('**/*.md')
-        .pipe(tap(markdownToHtml))
-        .pipe(gulp.dest('./dist'));
+        .pipe(markdown())
+        .pipe(gulp.dest(function(f) {
+            return f.base;
+        }));
 });
 
-function markdownToHtml(file) {
-    var result = md.render(file.contents.toString());
-    file.contents = new Buffer(result);
-    file.path = gutil.replaceExtension(file.path, '.html');
-    return;
-}
-
-gulp.task('watch', function() {
-    gulp.watch('**/*.md', ['build']);
+gulp.task('default', ['markdown'], function() {
+    gulp.watch('**/*.md', ['markdown']);
 });
